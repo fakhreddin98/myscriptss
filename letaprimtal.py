941620578477a619138926ef1ef0c0a7ac39f8c2
fakhreddin98 <fakhreddin.kabawe@icloud.com>
2022-09-08 14:43:00 +0100

    prim

--- letaprimtal.py
+++ letaprimtal.py
@@ -1,1 +1,14 @@
-testar
+#!/usr/bin/python
+
+numbersToTest = int (input('Hur många tal vill du undersöka'))
+
+
+for i in range(1,numbersToTest+1):
+ provenNotPrime = False
+ for k in range(2,i):
+  if i % k == 0:
+   print(f'{i} ej ett primtal')
+   provenNotPrime = True
+   break
+ if provenNotPrime == False:
+  print(f'*** {i} är ett Primtal! ***')
